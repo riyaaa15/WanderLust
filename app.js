eea53639456e4a1af4828586eef99dd9eae4da15
riyaaa15 <riyaredhu1020@gmail.com>
2026-07-19 17:35:20 +0530

    add error.ejs

--- app.js
+++ app.js
@@ -106,7 +106,8 @@ app.use("/{*splat}", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let {statusCode = 500, message = "Something went wrong!"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
