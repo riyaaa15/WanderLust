251e9054172aad496db7bc83edfbdb6158f49d31
riyaaa15 <riyaredhu1020@gmail.com>
2026-07-20 17:23:50 +0530

    add joifor schema validation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -53,8 +54,10 @@ app.get("/listings/:id", wrapAsync(async(req, res) => {
 
 //Create Route
 app.post("/listings", wrapAsync(async (req, res, next) => {
-    if(!req.body.listen) {
-        throw new ExpressError(400, "Send valid data for listing");
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if (result.error) {
+        throw new ExpressError(400, result.error);
     }
     const newListing  = new Listing(req.body.listing);
     await newListing.save();
