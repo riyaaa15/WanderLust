0bf9c272d3e2f5fbbc73841a926921555cad0581
riyaaa15 <riyaredhu1020@gmail.com>
2026-08-25 18:53:12 +0530

    handling: Delete Listing

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const review = require('./models/review');
 
 module.exports.listingSchema = Joi.object({
-    lisitng : Joi.object({
+    listing : Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required(),
