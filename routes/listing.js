d770854f3b5460ec20f8aea407370513dd9c6139
riyaaa15 <riyaredhu1020@gmail.com>
2026-09-02 15:59:04 +0530

    implement sessions and flash in prject

--- routes/listing.js
+++ routes/listing.js
@@ -39,6 +39,7 @@ router.post("/",
     wrapAsync(async (req, res, next) => {
     const newListing  = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 }));
 
@@ -54,6 +55,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 router.put("/:id", validateListing, wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -61,6 +63,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 router.delete("/:id", wrapAsync(async(req, res) => {
     let {id} = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 }));
 
