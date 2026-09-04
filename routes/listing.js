427f216cb58b7762302afb255b79ce165caf2657
riyaaa15 <riyaredhu1020@gmail.com>
2026-09-04 11:28:21 +0530

    add flash success and failure includes

--- routes/listing.js
+++ routes/listing.js
@@ -30,6 +30,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async(req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing) {
+         req.flash("error", "Listing you requested for does not exist!");
+         return res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -48,6 +52,10 @@ router.post("/",
 router.get("/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
+    if(!listing) {
+         req.flash("error", "Listing you requested for does not exist!");
+         return res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
