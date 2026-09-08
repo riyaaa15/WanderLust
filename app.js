c6d7fc2788d02d33f12f7164cd09e28654e8aa1d
riyaaa15 <riyaredhu1020@gmail.com>
2026-09-08 21:34:02 +0530

    add user demo

--- app.js
+++ app.js
@@ -64,6 +64,16 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student"
+    });
+
+   let registeredUser = await User.register(fakeUser, "helloworld");
+   res.send(registeredUser);
 })
 
 app.use("/listings", listings);
