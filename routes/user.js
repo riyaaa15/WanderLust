16494e351bd8283a3722890ba2771cd9782e0b12
riyaaa15 <riyaredhu1020@gmail.com>
2026-09-25 16:12:19 +0530

    add MVC for reviews and users

--- routes/user.js
+++ routes/user.js
@@ -5,33 +5,13 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup", (req, res) => {
-    res.render("users/signup.ejs");
-});
+const userController = require("../controllers/users.js");
 
-router.post("/signup", wrapAsync(async(req, res, next) => {
-    try {
-        let {username, email, password} = req.body;
-        const newUser = new User({email, username});
-        const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.login(registeredUser, (err) => {
-            if(err) {
-                return next(err);
-            }
-            req.flash("success", "Welcome to Wanderlsut!");
-            res.redirect("/listings");
-        })
-    } catch(e) {
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
-    
-}));
+router.get("/signup", userController.renderSignupForm);
 
-router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
-});
+router.post("/signup", wrapAsync(userController.signup));
+
+router.get("/login", userController.renderLoginForm);
 
 router.post("/login", 
     saveRedirectUrl,
@@ -39,21 +19,9 @@ router.post("/login",
         failureRedirect: '/login', 
         failureFlash: true
     }), 
-    async(req, res) => {
-    req.flash("success", "Welcome back to Wanderlust!");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-    }
+    userController.login
 );
 
-router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if(err) {
-            return next(err);
-        }
-        req.flash("success", "you are logged out now!");
-        res.redirect("/listings");
-    })
-})
+router.get("/logout", userController.logout);
 
 module.exports = router;
